2541b82fe39f07b9ca21dec5333dcd2e884fe5f1
Victor Lattaro Volpini <lattaro@pop-os.home>
2024-09-28 13:50:57 +0200

    added tree-sitter to python and cc modes

--- init.el
+++ init.el
@@ -115,7 +115,8 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;(cc +lsp)         ; C > C++ == 1
+       (cc +lsp            ; C > C++ == 1
+           +tree-sitter)
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -160,7 +161,8 @@
        ;;purescript        ; javascript, but functional
        (python +lsp        ; is better than ugly
                +conda
-               +ein)
+               +ein
+               +tree-sitter)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
