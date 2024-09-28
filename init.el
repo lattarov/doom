7e05480983e62061f386e627359ca112c7eb9481
Victor Lattaro Volpini <lattaro@pop-os.home>
2024-09-28 19:09:27 +0200

    feat: added layout package +azerty flag

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        ;;japanese
-       ;;layout            ; auie,ctsrnm is the superior home row
+       (layout +azerty)   ; auie,ctsrnm is the superior home row
 
        :completion
        ;; company           ; the ultimate code completion backend
