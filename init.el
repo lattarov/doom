7a17874a9853345a7ff7310bb564636b56c1c1c1
Victor LATTARO VOLPINI <v.lattaro-volpini.ext@tiama.com>
2025-08-21 09:39:11 +0200

    feat: add csharp

--- init.el
+++ init.el
@@ -124,7 +124,9 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       (csharp +lsp        ; unity, .NET, and mono shenanigans
+               +tree-sitter
+               +dotnet)
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
