8c36a1aea6a66eb9775b3889528c1fdc291c97b8
Victor LATTARO VOLPINI <v.lattaro-volpini.ext@tiama.com>
2026-07-20 17:08:20 +0200

    chore(init): reduce diffs with init.example.el
    report the scad module from doomemacs/core $EMACSDIR/static/init.example.el

--- init.el
+++ init.el
@@ -197,6 +197,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp          ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
              +tree-sitter)
+       ;;scad              ; trust the preview, regret the render
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
