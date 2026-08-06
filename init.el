b64273ce764c769a9e30c12ef12fe1a71a98237f
Victor LATTARO VOLPINI <v.lattaro-volpini.ext@tiama.com>
2026-08-06 10:29:12 +0200

    feat(window-select): add +numbers and +switch-window

--- init.el
+++ init.el
@@ -54,7 +54,8 @@
        unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
+       (window-select +numbers ; visually switch windows
+                      +switch-window)
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
